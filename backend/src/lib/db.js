5df3349fe69511434604d5305a7c151fc4c7398c
Michael Hull <Michaelhullzero@gmail.com>
2026-04-06 04:34:04 -0700

    Added access to mongoDB and updated the README. To establish a connection I am bypassing the windows DNS resolver in the top of server.js

---
+++ backend/src/lib/db.js
@@ -0,0 +1,15 @@
+// This is the main file used to connect to the mongoDB database
+import mongoose from "mongoose"
+
+export const connectDB = async () => {
+    //console.log("DEBUG: MONGO_URL is", process.env.MONGO_URL); // Add this!
+    try {
+        const mongoConnection = await mongoose.connect(process.env.MONGO_URL);
+        console.log("MongoDB has established a connection: ", mongoConnection.connection.host);
+    } catch (error) {
+        console.error("MongoDB could not establish a connection");
+        console.error("Error Name:", error.name);
+        console.error("Error Message:", error.message);
+        process.exit(1);
+    }
+}
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,15 +1,19 @@
+// this bypasses Windows DNS resolver for mongoose to establish a connection to mongoDB
+import dns from "node:dns";
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
 // This is using the express package to run the backend
 import express from "express";
 // This is used to access the .env variables to avoid needing to hardcode them
 import dotenv from "dotenv";
+dotenv.config();
 // used for running the backend and frontend at the same time with one command.
 import path from "path";
 // import the routes from the auth.route.js file
 import authRoutes from "./routes/auth.route.js";
 // import the routes from the message.route.js file
 import messageRoutes from "./routes/message.route.js";
-
-dotenv.config();
+// import connectDB from the db.js file
+import { connectDB } from "./lib/db.js"
 
 const app = express();
 const __dirname = path.resolve();
@@ -27,4 +31,7 @@ if (process.env.NODE_ENV === "production" ){
     });
 }
 
-app.listen(PORT, () => console.log("Server is operational and running on port: " + PORT));
+app.listen(PORT, () => {
+    console.log("Server is operational and running on port: " + PORT);
+    connectDB();
+});
